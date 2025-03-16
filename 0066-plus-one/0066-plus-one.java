971f600bfeb33f771a2dc9c2f7fd2711b4d911b3
Abinash Nayak <nayakabinash1007@gmail.com>
2025-03-16 20:41:44 +0530

    Time: 0 ms (100%), Space: 42.1 MB (30.26%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,8 +1,6 @@
 class Solution {
     public int[] plusOne(int[] digits) {
         int n = digits.length;
-
-        // for same array length
         for (int i = n - 1; i >= 0; i--) {
             if (digits[i] < 9) {
                 digits[i]++;
@@ -10,12 +8,9 @@ class Solution {
             }
             digits[i] = 0;
         }
-
-        // for different array length
-        int[] newNum = new int[n + 1];
-        for (int i = 0; i < n + 1; i++) {
-            newNum[0] = 1;
-        }
-        return newNum;
+        // If all digits are 9
+        int[] newDigit = new int[n + 1];
+        newDigit[0] = 1;
+        return newDigit;
     }
 }

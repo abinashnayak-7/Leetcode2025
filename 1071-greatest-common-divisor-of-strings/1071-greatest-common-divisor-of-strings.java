9a4518e6cf7bcb8982695b1782612f0ddb5b16f3
Abinash Nayak <nayakabinash1007@gmail.com>
2025-03-29 19:42:28 +0530

    Time: 0 ms (100%), Space: 42.6 MB (24.11%) - LeetHub

---
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
@@ -0,0 +1,18 @@
+class Solution {
+    public static int gcd(int num1, int num2) {
+        while (num2 != 0) {
+            int temp = num2;
+            num2 = num1 % num2;
+            num1 = temp;
+        }
+        return num1;
+    }
+
+    public String gcdOfStrings(String str1, String str2) {
+        if (!(str1 + str2).equals(str2 + str1)) {
+            return "";
+        }
+        int gcd = gcd(str1.length(), str2.length());
+        return str1.substring(0, gcd);
+    }
+}

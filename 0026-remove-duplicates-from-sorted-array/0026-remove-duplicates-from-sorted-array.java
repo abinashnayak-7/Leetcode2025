bf37c5e2afc2a1c0f8810f3831ac457da42fa6fd
Abinash Nayak <nayakabinash1007@gmail.com>
2025-04-17 23:15:20 +0530

    Time: 0 ms (100%), Space: 45 MB (23.69%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,18 +1,31 @@
+// class Solution {
+//     public int removeDuplicates(int[] nums) {
+//       if(nums.length == 0){ //edge case
+//             return 0;
+//         }
+//         int n = nums.length;
+//         int k = 0;
+//         for(int i = 0; i < n; i++){
+//             if(nums[i] != nums[k]){
+//                 k++;
+//                 nums[k] = nums[i]; 
+//             }
+//         }
+//         return k + 1;
+//     }
+// }
+
+
 class Solution {
     public int removeDuplicates(int[] nums) {
-      if(nums.length == 0){ //edge case
-            return 0;
-        }
-        int n = nums.length;
-        int k = 0;
-        for(int i = 0; i < n; i++){
-            if(nums[i] != nums[k]){
-                k++;
-                nums[k] = nums[i]; 
+        int j = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[j] = nums[i];
+                j++;
             }
         }
-        return k + 1;
+        return j;
     }
 }
-
  

0814a87762ebe2ea7c379ca7c0d3de9b079a5fae
Abinash Nayak <nayakabinash1007@gmail.com>
2025-05-02 16:35:04 +0530

    Time: 2 ms (84.05%), Space: 45.9 MB (81.18%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,50 +1,17 @@
-// class Solution {
-//     public void reverse(int[] nums, int start, int end) {
-//         start = 0;
-//         end = nums.length - 1;
-
-//         while (start <= end) {
-//             int temp = nums[start];
-//             nums[start] = nums[end];
-//             nums[end] = temp;
-//         }
-//         start++;
-//         end--;
-//     }
-
-//     public void moveZeroes(int[] nums) {
-//         int n = nums.length;
-//         Arrays.sort(nums);
-//         reverse(nums, 0, n - 1);
-
-//         int count = 0;
-//         for (int i = 0; i < n; i++) {
-//             if (nums[i] == 0) {
-//                 break;
-//             } else {
-//                 count++;
-//             }
-//         }
-
-//         reverse(nums, 0, count - 1);
-//     }
-// }  TIME LIMIT EXCEED
-
-
 class Solution {
     public void moveZeroes(int[] nums) {
-        int nonZeroIndex = 0; // Index to place the next non-zero element
+        int n = nums.length;
+        int nonZeroIdx = 0;
 
-        // Step 1: Move all non-zero elements to the front
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != 0) {
-                nums[nonZeroIndex] = nums[i];
-                nonZeroIndex++;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != 0) { //check number is not zero
+                nums[nonZeroIdx] = nums[i];
+                nonZeroIdx++;
             }
         }
 
-        // Step 2: Fill the rest of the array with zeros
-        for (int i = nonZeroIndex; i < nums.length; i++) {
+        //for filling zeroes at the end
+        for (int i = nonZeroIdx; i < n; i++) {
             nums[i] = 0;
         }
     }

1f36dd976b50643f5fc9d58c827266961ba370bf
Abinash Nayak <nayakabinash1007@gmail.com>
2025-05-13 21:37:03 +0530

    Time: 1 ms (99.31%), Space: 44.6 MB (92.56%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+
+        int[] frequency = new int[26];
+        for (char c : magazine.toCharArray()) {
+            frequency[c - 'a']++;
+        }
+
+        for (char c : ransomNote.toCharArray()) {
+            if (frequency[c - 'a'] == 0) {
+                return false;
+            }
+            frequency[c - 'a']--;
+        }
+        return true;
+    }
+}

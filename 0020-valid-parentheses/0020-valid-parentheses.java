2960d2ddc3c3b7a61f93f1079e3fea6f47950961
Abinash Nayak <nayakabinash1007@gmail.com>
2025-06-03 23:53:51 +0530

    Time: 2 ms (97.27%), Space: 41.9 MB (48.11%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,8 +1,35 @@
 class Solution {
     public boolean isValid(String s) {
-        while (s.contains("()") || s.contains("{}") || s.contains("[]")) {
-            s = s.replace("()", "").replace("{}", "").replace("[]", "");
+        // while (s.contains("()") || s.contains("{}") || s.contains("[]")) {
+        //     s = s.replace("()", "").replace("{}", "").replace("[]", "");
+        // }
+        // return s.isEmpty();
+
+        int n = s.length();
+        if (n % 2 != 0) {
+            return false;
         }
-        return s.isEmpty();
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            char ch = s.charAt(i);
+            if (ch == '(' || ch == '[' || ch == '{') {
+                stack.push(ch);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char top = stack.peek();
+                if (ch == ')' && top != '(') {
+                    return false;
+                } else if (ch == '}' && top != '{') {
+                    return false;
+                } else if (ch == ']' && top != '[') {
+                    return false;
+                } else {
+                    stack.pop();
+                }
+            }
+        }
+        return (stack.size() == 0);
     }
 }

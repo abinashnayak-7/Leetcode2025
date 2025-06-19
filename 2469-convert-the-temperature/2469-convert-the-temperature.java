ab22a9893021cfebbd0c1554bd67593ef6e59049
Abinash Nayak <nayakabinash1007@gmail.com>
2025-06-19 22:30:22 +0530

    Time: 0 ms (100%), Space: 41.8 MB (76.01%) - LeetHub

---
+++ 2469-convert-the-temperature/2469-convert-the-temperature.java
@@ -0,0 +1,9 @@
+class Solution {
+    public double[] convertTemperature(double celsius) {
+        double[] temperature = new double[2];
+        temperature[0] = celsius + 273.15;
+        temperature[1] = (celsius * 1.80) + 32.00;
+
+        return temperature;
+    }
+}
